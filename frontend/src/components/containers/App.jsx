b614befb22bb9cdd5fc19657b374db92cde8e257
Neil Poulin <neil.j.poulin@gmail.com>
2017-08-24 08:53:08 -0600

    update nav more

--- frontend/src/components/containers/App.jsx
+++ frontend/src/components/containers/App.jsx
@@ -57,8 +57,7 @@ const App = () => (
   <Router>
     <div className="container">
       <NavBar/>
-      <div className="pageBase">
-          <a href="https://slack.com/oauth/authorize?&client_id=225772115667.227177070210&scope=incoming-webhook,chat:write:bot,channels:write,channels:read,files:write:user"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+      <div className="pageBase">          
           <Route exact path="/" component={LoginPage}/>
           <Route path="/login" component={LoginPage}/>
           <Route path="/topics" component={Topics}/>
--- frontend/src/components/containers/NavBar.jsx
+++ frontend/src/components/containers/NavBar.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import {
     BrowserRouter as Router,
     Route,
-    Link
+    Link,
+    NavLink
 } from 'react-router-dom'
 
 class NavBar extends React.Component {
     render () {
         return <div className="navContainer">
             <ul className="nav">
-                <li><Link to="/">Home Page</Link></li>
-                <li><Link to="/login">Login</Link></li>
+                <li className='navLink'><Link to="/">OneRoost</Link></li>
+                <li className='navLink'><NavLink to="/login">Login</NavLink></li>
+
+            </ul>
+            <ul className='nav'>
+                <li>
+                    <a href="https://slack.com/oauth/authorize?&client_id=225772115667.227177070210&scope=incoming-webhook,chat:write:bot,channels:write,channels:read,files:write:user"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+                </li>
             </ul>
         </div>
     }
